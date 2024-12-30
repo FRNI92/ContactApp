fe047b971fa912b166ade479db67fa72c9267e85
fredrik <freedrik92@hotmail.com>
2024-12-30 12:33:32 +0100

    Har gjort iordning 2 tester. 1 för create och en för add

--- ContactApp_Tests/Services_Tests/ContactService_Tests.cs
+++ ContactApp_Tests/Services_Tests/ContactService_Tests.cs
@@ -11,24 +11,36 @@ public class ContactService_Tests
     private readonly Mock<IContactService> _contactServiceMock;
     private readonly IContactService _contactService;
 
+    private readonly Mock<IFileService> _fileServiceMock;
+    private readonly IFileService _fileService;
+
+    private readonly Mock<IContactFactory> _contactFactoryMock;
+    private readonly IContactFactory _contactFactory;
     public ContactService_Tests()
     {
         _contactServiceMock = new Mock<IContactService>();
         _contactService = _contactServiceMock.Object;
+
+        _fileServiceMock = new Mock<IFileService>();
+        _fileService = _fileServiceMock.Object;
+
+        _contactFactoryMock = new Mock<IContactFactory>();
+        _contactFactory = _contactFactoryMock.Object;
+
     }
 
     [Fact]
     public void Create_Contact_ShouldSetCorrectValues()
     {
-        //Arrrange
+        //Arrrange, använder delvis mock av Icontactservice och delvis riktiga Contactservice
         var contact = new Contact { FirstName = "Fredrik", LastName = "Nilsson", Email = "Fred@domain.com",
-                                    PhoneNumber = "123456789", StreetAdress = "Villavägen 12", PostalCode = "202 27",
-                                    City = "Stockholm"}; 
+            PhoneNumber = "123456789", StreetAdress = "Villavägen 12", PostalCode = "202 27",
+            City = "Stockholm" };
         _contactServiceMock
             .Setup(cs => cs.Create())
             .Returns(contact);
         //Act
-        var result =_contactService.Create();
+        var result = _contactService.Create();
         //Assert
         Assert.NotNull(result);
         Assert.Equal("Fredrik", result.FirstName);
@@ -39,6 +51,37 @@ public class ContactService_Tests
         Assert.Equal("202 27", result.PostalCode);
         Assert.Equal("Stockholm", result.City);
     }
+
+
+    [Fact]
+    public void Add_ShouldAddToList_AndSaveToFile()
+    {
+        //arrange
+        _fileServiceMock.Setup(fs => fs.LoadFromFile()).Returns(new List<Contact>());//ville inte ändra koden jag hade så jag instansierar listan såhär
+        
+        var mockContact = new Contact { FirstName = "Fredrik", LastName = "Nilsson" };
+
+        _contactFactoryMock.Setup(contactfactory => contactfactory.Create()).Returns(mockContact);
+
+        var contactService = new ContactService(_contactFactoryMock.Object, _fileServiceMock.Object);
+        //act
+        contactService.Add(mockContact);
+        //Assert
+        _fileServiceMock.Verify(fs => fs.SaveToFile(It.IsAny<List<Contact>>()), Times.Once);//dubbelkollar att den metoden användes
+        var result = contactService.Read();// kollar att det ligger någon i listan
+        Assert.Contains(mockContact, result);
+
+
+        //var savedContact = result.FirstOrDefault(c => c.FirstName == "Fredrik" && c.LastName == "Nilsson");
+        //Assert.NotNull(savedContact);
+        //Assert.Equal("Fredrik", savedContact.FirstName);
+        //Assert.Equal("Nilsson", savedContact.LastName);
+
+
+    }
+
+    [Fact]
+    pu
 }
 
 
